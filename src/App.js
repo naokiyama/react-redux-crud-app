6cb1a89df7f94f7a17e0effd25544b1a089e7784
naokiyamaguchi <naokiyamaguchi@gmail.com>
2021-05-11 11:52:46 +0900

    create class Component and functionl component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import propTypes from 'prop-types'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserProfile name={"naoki"} age={12} description={"hello"} />
     </div>
   );
+}
+
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <table>
+        <tr>
+          <td>Name:</td>
+          <td>{props.name}</td>
+        </tr>
+        <tr>
+          <td>Age:</td>
+          <td>{props.age}</td>
+        </tr>
+        <tr>
+          <td>Description:</td>
+          <td>{props.description}</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+UserProfile.propTypes = {
+  name: propTypes.string,
+  age: propTypes.number.isRequired,
+  description: propTypes.string
 }
 
 export default App;
