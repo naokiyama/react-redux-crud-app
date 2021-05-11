a44af20db251153b9f110d0f9cd1156827577123
naokiyamaguchi <naokiyamaguchi@gmail.com>
2021-05-11 13:16:44 +0900

    create props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import propTypes from 'prop-types'
-function App() {
-  return (
-    <div>
-      <UserProfile name={"naoki"} age={12} description={"hello"} />
-    </div>
-  );
-}
+import React, { Component } from 'react';
 
-const UserProfile = (props) => {
-  return (
-    <div>
-      <h1>Profile</h1>
-      <table>
-        <tr>
-          <td>Name:</td>
-          <td>{props.name}</td>
-        </tr>
-        <tr>
-          <td>Age:</td>
-          <td>{props.age}</td>
-        </tr>
-        <tr>
-          <td>Description:</td>
-          <td>{props.description}</td>
-        </tr>
-      </table>
-    </div>
-  )
-}
+const App = () => {
+  <Counter></Counter>
+};
 
-/*propTypesで型定義をすることができる*/
-UserProfile.propTypes = {
-  name: propTypes.string,
-  age: propTypes.number.isRequired,
-  description: propTypes.string
+class Counter extends Component{
+  render(){
+  return (<div>counter</div>)
+  }
 }
-
 
 export default App; 
